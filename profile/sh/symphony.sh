2793026b90c3616ce204ee842323c4242f755ac5
Steve Norledge <steve.norledge@alces-software.com>
2014-10-04 17:21:04 +0100

    - cobbler base files for centos7 and profile tweaks

---
+++ profile/sh/symphony.sh
@@ -0,0 +1,33 @@
+################################################################################
+# (c) Copyright 2007-2014 Alces Software Ltd                                   #
+#                                                                              #
+# Symphony Software Toolkit                                                    #
+#                                                                              #
+# This file/package is part of Symphony                                        #
+#                                                                              #
+# Symphony is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU Affero General Public License as published by the Free  #
+# Software Foundation, either version 3 of the License, or (at your option)    #
+# any later version.                                                           #
+#                                                                              #
+# Symphony is distributed in the hope that it will be useful, but WITHOUT      #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License #
+# for more details.                                                            #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with Symphony.  If not, see <http://www.gnu.org/licenses/>.            #
+#                                                                              #
+# For more information on the Symphony Toolkit, please visit:                  #
+# http://www.alces-software.org/symphony                                       #
+#                                                                              #
+################################################################################
+export SYMPHONY_HOME=/var/lib/symphony/
+
+if [ -d "${SYMPHONY_HOME}/profile/sh/profile.d/" ]; then
+  for a in "${SYMPHONY_HOME}/profile/sh/profile.d/"/*.sh; do
+      if [ -r "$a" ]; then
+	  source $a
+      fi
+  done
+fi
