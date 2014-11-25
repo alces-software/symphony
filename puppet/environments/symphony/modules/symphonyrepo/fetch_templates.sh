98c4d4d21ce61a9325b0c6dcaeac2a77ec9aa2bc
Steve Norledge <steve.norledge@alces-software.com>
2014-11-25 13:36:19 +0000

    - revert to master version

--- puppet/environments/symphony/modules/symphonyrepo/fetch_templates.sh
+++ puppet/environments/symphony/modules/symphonyrepo/fetch_templates.sh
@@ -24,7 +24,7 @@
 #                                                                              #
 ################################################################################
 
-SYMPHONY_REPO_URL="http://repo.alces-software.com/configs/"
+SYMPHONY_REPO_URL="http://symphony-repo/configs/"
 
 cd `dirname $0`
 if [ ! -d templates/yum ]; then
