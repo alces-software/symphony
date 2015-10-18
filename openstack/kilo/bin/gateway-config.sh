ed719aed011c1f120014e3dbe936b00f45d4fca6
root <root@symphony-gateway.mgt.symphony.local>
2015-10-18 18:57:13 +0100

    - removed keystone update

--- openstack/kilo/bin/gateway-config.sh
+++ openstack/kilo/bin/gateway-config.sh
@@ -6,7 +6,6 @@ echo "Press any key to continue.."; read
 . /var/lib/symphony/openstack/kilo/bin/vars
 
 #Fixup Configs
-sh /var/lib/symphony/openstack/kilo/bin/install.sh controller/etc/keystone/keystone.conf /etc/keystone/keystone.conf
 sh /var/lib/symphony/openstack/kilo/bin/install.sh network/etc/neutron/dhcp_agent.ini /etc/neutron/dhcp_agent.ini
 sh /var/lib/symphony/openstack/kilo/bin/install.sh network/etc/neutron/neutron.conf /etc/neutron/neutron.conf
 sh /var/lib/symphony/openstack/kilo/bin/install.sh network/etc/neutron/metadata_agent.ini /etc/neutron/metadata_agent.ini
